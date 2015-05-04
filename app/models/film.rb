03b180b43805a536c1113df193919853ac5891e8
max <max@iMac.local>
2015-05-05 00:33:20 +0300

    Working badges, add bootstrap js

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,5 @@
 class Film < ActiveRecord::Base
   validates :name, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 11 }
+  validates :year, numericality: { only_integer: true, greater_than: 1940, less_than: Time.new.year+1 }
 end
